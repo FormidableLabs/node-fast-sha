ede78220281550b21132172cbe91f761f369bea2
Grant Sander <gksander93@gmail.com>
2023-07-26 06:22:28 -0500

    first commit

---
+++ play.js
@@ -0,0 +1,7 @@
+const { sha512 } = require("./index");
+
+const main = () => {
+  console.log(sha512("hello world!"));
+};
+
+main();
---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![deny(clippy::all)]
+use sha2::{Digest, Sha256, Sha512};
+
+#[macro_use]
+extern crate napi_derive;
+
+#[napi]
+pub fn sha512(input: String) -> String {
+  let mut hasher = Sha512::new();
+  hasher.update(input);
+  let result = hasher.finalize();
+  format!("{:x}", result)
+}
+
+#[napi]
+pub fn sha256(input: String) -> String {
+  let mut hasher = Sha256::new();
+  hasher.update(input);
+  let result = hasher.finalize();
+  format!("{:x}", result)
+}
