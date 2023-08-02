dcc1c9cc515e793ad08636c6b89280c8703e218e
Grant Sander <gksander93@gmail.com>
2023-08-02 15:19:00 -0500

    Install tests

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,17 @@
-import test from 'ava'
+import test from "ava";
 
-import { sha256 } from '../lib/index.mjs'
+import { sha256, sha512 } from "../lib/index.mjs";
 
-test('sha256 from native', (t) => {
-  t.is(typeof sha256("Hello world!"), "string")
-})
+test("sha256 from native", (t) => {
+  t.is(
+    sha256("Hello world!"),
+    "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a",
+  );
+});
+
+test("sha512 from native", (t) => {
+  t.is(
+    sha512("Hello world!"),
+    "f6cde2a0f819314cdde55fc227d8d7dae3d28cc556222a0a8ad66d91ccad4aad6094f517a2182360c9aacf6a3dc323162cb6fd8cdffedb0fe038f55e85ffb5b6 DELETE ME",
+  );
+});
