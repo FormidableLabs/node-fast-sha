9feb1a9d85aa1aaae73effc63d653cf8dde5ff8a
Ryan Roemer <ryan.roemer@formidable.com>
2023-08-07 09:10:56 -0700

    [DO NOT MERGE][EXPERIMENT] Experiment with buffer/typedarrays (#14)
    * Experiment with buffer
    
    * sha256_buf with benchmarks
    
    * RM
    
    ---------
    
    Co-authored-by: Grant Sander <gksander93@gmail.com>

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,6 +1,6 @@
 import test from "ava";
 
-import { sha256, sha512 } from "../lib/index.mjs";
+import { sha256, sha256Buf, sha512, sha512Buf } from "../lib/index.mjs";
 
 test("sha256 from native", (t) => {
   t.is(
@@ -13,5 +13,20 @@ test("sha512 from native", (t) => {
   t.is(
     sha512("Hello world!"),
     "f6cde2a0f819314cdde55fc227d8d7dae3d28cc556222a0a8ad66d91ccad4aad6094f517a2182360c9aacf6a3dc323162cb6fd8cdffedb0fe038f55e85ffb5b6",
+  );
+});
+
+// TODO: REMOVE
+test("sha512Buf from native", (t) => {
+  t.is(
+    sha512Buf(Buffer.from("Hello world!")),
+    "f6cde2a0f819314cdde55fc227d8d7dae3d28cc556222a0a8ad66d91ccad4aad6094f517a2182360c9aacf6a3dc323162cb6fd8cdffedb0fe038f55e85ffb5b6",
+  );
+});
+
+test("sha256Buf from native", (t) => {
+  t.is(
+    sha256Buf(Buffer.from("Hello world!")),
+    "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a",
   );
 });
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 #![deny(clippy::all)]
+use std::borrow::Cow;
+use napi::bindgen_prelude::Buffer;
 use sha2::{Digest, Sha256, Sha512};
 
 #[macro_use]
@@ -12,10 +14,32 @@ pub fn sha512(input: String) -> String {
   format!("{:x}", result)
 }
 
+// TODO: REMOVE
+#[napi]
+pub fn sha512_buf(buf: Buffer) -> String {
+  let mut hasher = Sha512::new();
+  let input: Cow<'_, str> = String::from_utf8_lossy(&buf);
+
+  hasher.update(input.into_owned());
+  let result = hasher.finalize();
+  format!("{:x}", result)
+}
+
 #[napi]
 pub fn sha256(input: String) -> String {
   let mut hasher = Sha256::new();
   hasher.update(input);
+  let result = hasher.finalize();
+  format!("{:x}", result)
+}
+
+// TODO: REMOVE
+#[napi]
+pub fn sha256_buf(buf: Buffer) -> String {
+  let mut hasher = Sha256::new();
+  let input: Cow<'_, str> = String::from_utf8_lossy(&buf);
+
+  hasher.update(input.into_owned());
   let result = hasher.finalize();
   format!("{:x}", result)
 }
